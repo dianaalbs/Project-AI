1e38ba6751c3d1adfdde1d8ac1949df6270e730e
dianaalbs <dianaadilla@student.uns.ac.id>
2023-06-07 15:35:24 +0700

    sss

--- tes3333.php
+++ tes3333.php
@@ -6,6 +6,3 @@ hhhh
 HAII
 aiss tes
 
-Permisi afif sorry ganggu, mau nanya sekalian minta tolong boleh?
-kan kita buat sc dfs buat projek AI, nah masih ada salah tpi gatau dimana, 
-dikasih input apapun outputnya ttp 'semua simpul terhubung' boleh minta tolong koreksi ga ya?
