80e26b79f7ebc9e7103b2b514e3861885250a6de
dianaalbs <dianaadilla@student.uns.ac.id>
2023-06-07 15:32:53 +0700

    tess

--- tes3333.php
+++ tes3333.php
@@ -6,6 +6,6 @@ hhhh
 HAII
 aiss tes
 
-sorry ganggu, mau nanya sekalian minta tolong boleh?
+Permisi afif sorry ganggu, mau nanya sekalian minta tolong boleh?
 kan kita buat sc dfs buat projek AI, nah masih ada salah tpi gatau dimana, 
-dikasih input apapun outputnya ttp 'semua simpul terhubung' boleh tolongin koreksi ga?
+dikasih input apapun outputnya ttp 'semua simpul terhubung' boleh minta tolong koreksi ga ya?
