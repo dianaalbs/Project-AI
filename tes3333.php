a5c5208420e738462dd1e3090cbf48ea231a67ba
asyahra19 <asyahrara02@student.uns.ac.id>
2023-06-07 15:30:08 +0700

    chat

--- tes3333.php
+++ tes3333.php
@@ -5,3 +5,7 @@ HALOOOOOOOOOOO
 hhhh
 HAII
 aiss tes
+
+sorry ganggu, mau nanya sekalian minta tolong boleh?
+kan kita buat sc dfs buat projek AI, nah masih ada salah tpi gatau dimana, 
+dikasih input apapun outputnya ttp 'semua simpul terhubung' boleh tolongin koreksi ga?
