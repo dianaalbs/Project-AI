2c794daba78cadc46bc7a2e29f043db19cfc7243
asyahra19 <asyahrara02@student.uns.ac.id>
2023-06-08 18:20:07 +0700

    startnodepost

--- tes6.php
+++ tes6.php
@@ -250,100 +250,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $graph->addEdge('29', '25');
 
     // Memanggil metode DFS dengan simpul awal yang dikirim melalui form
-    $startNode1 = $_POST['1'];
-    $startNode2 = $_POST['2'];
-    $startNode3 = $_POST['3 '];
-    $startNode4 = $_POST['4'];
-    $startNode5 = $_POST['5'];
-    $startNode6 = $_POST['6'];
-    $startNode7 = $_POST['7'];
-    $startNode8 = $_POST['8'];
-
-    $startNode9  = $_POST['9'];
-    $startNode10 = $_POST['10'];
-    $startNode11 = $_POST['11'];
-    $startNode12 = $_POST['12'];
-    $startNode13 = $_POST['13'];
-    $startNode14 = $_POST['14'];
-    $startNode15 = $_POST['15'];
-
-    $startNode16 = $_POST['16'];
-    $startNode17 = $_POST['17'];
-    $startNode18 = $_POST['18'];
-    $startNode19 = $_POST['19'];
-    $startNode20 = $_POST['20'];
-    $startNode21 = $_POST['21'];
-    $startNode22 = $_POST['22'];
-
-    $startNode23 = $_POST['23'];
-    $startNode24 = $_POST['24'];
-    $startNode25 = $_POST['25'];
-    $startNode26 = $_POST['26'];
-    $startNode27 = $_POST['27'];
-    $startNode28 = $_POST['28'];
-
-    $startNode29 = $_POST['29'];
-    $startNode30 = $_POST['30'];
-    $startNode31 = $_POST['31'];
-    $startNode32 = $_POST['32'];
-
-    $startNode33 = $_POST['33'];
-    $startNode34 = $_POST['34'];
-    $startNode35 = $_POST['35'];
-    $startNode36 = $_POST['36'];
-
-    $startNode37 = $_POST['37'];
-    $startNode38 = $_POST['38'];
-    $startNode39 = $_POST['39'];
-    $startNode40 = $_POST['40'];
-    $startNode41 = $_POST['41'];
-    $startNode42 = $_POST['42'];
-    $startNode43 = $_POST['43'];
-    $startNode44 = $_POST['44'];
-    $startNode45 = $_POST['45'];
-    $startNode46 = $_POST['46'];
-    $startNode47 = $_POST['47'];
-
-    $startNode48 = $_POST['48'];
-    $startNode49 = $_POST['49'];
-    $startNode50 = $_POST['50'];
-
-    $startNode51 = $_POST['51'];
-    $startNode52 = $_POST['52'];
-    $startNode53 = $_POST['53'];
-    $startNode54 = $_POST['54'];
-
-    $startNode55 = $_POST['55'];
-    $startNode56 = $_POST['56'];
-    $startNode57 = $_POST['57'];
-    $startNode58 = $_POST['58'];
-    $startNode59 = $_POST['59'];
-    $startNode60 = $_POST['60'];
-    $startNode61 = $_POST['61'];
-    $startNode62 = $_POST['62'];
-    $startNode63 = $_POST['63'];
-    $startNode64 = $_POST['64'];
-    $startNode65 = $_POST['65'];
-    $startNode66 = $_POST['66'];
-
-    $startNode67 = $_POST['67'];
-    $startNode68 = $_POST['68'];
-    $startNode69 = $_POST['69'];
-
-    $startNode70 = $_POST['70'];
-    $startNode70 = $_POST['70'];
-    $startNode71 = $_POST['71'];
-    $startNode72 = $_POST['72'];
-    $startNode73 = $_POST['73'];
-
-    $startNode74 = $_POST['74'];
-    $startNode75 = $_POST['75'];
-    $startNode76 = $_POST['76'];
-    $startNode77 = $_POST['77'];
-    $startNode78 = $_POST['78'];
-    $startNode79 = $_POST['79'];
-
-    $startNode80 = $_POST['80'];
+    $startNode1 = $_POST['startNode1'];
+    $startNode2 = $_POST['startNode2'];
+    $startNode3 = $_POST['startNode3'];
+    $startNode4 = $_POST['startNode4'];
+    $startNode5 = $_POST['startNode5'];
+    $startNode6 = $_POST['startNode6'];
+    $startNode7 = $_POST['startNode7'];
+    $startNode8 = $_POST['startNode8'];
+    $startNode9  = $_POST['startNode9'];
+    $startNode10 = $_POST['startNode10'];
+    $startNode11 = $_POST['startNode11'];
+    $startNode12 = $_POST['startNode12'];
+    $startNode13 = $_POST['startNode13'];
+    $startNode14 = $_POST['startNode14'];
+    $startNode15 = $_POST['startNode15'];
+    $startNode16 = $_POST['startNode16'];
+    $startNode17 = $_POST['startNode17'];
+    $startNode18 = $_POST['startNode18'];
+    $startNode19 = $_POST['startNode19'];
+    $startNode20 = $_POST['startNode20'];
+    $startNode21 = $_POST['startNode21'];
+    $startNode22 = $_POST['startNode22'];
+    $startNode23 = $_POST['startNode23'];
+    $startNode24 = $_POST['startNode24'];
+    $startNode25 = $_POST['startNode25'];
+    $startNode26 = $_POST['startNode26'];
+    $startNode27 = $_POST['startNode27'];
+    $startNode28 = $_POST['startNode28'];
+    $startNode29 = $_POST['startNode29'];
+    $startNode30 = $_POST['startNode30'];
+    $startNode31 = $_POST['startNode31'];
+    $startNode32 = $_POST['startNode32'];
+    $startNode33 = $_POST['startNode33'];
+    $startNode34 = $_POST['startNode34'];
+    $startNode35 = $_POST['startNode35'];
+    $startNode36 = $_POST['startNode36'];
+    $startNode37 = $_POST['startNode37'];
+    $startNode38 = $_POST['startNode38'];
+    $startNode39 = $_POST['startNode39'];
+    $startNode40 = $_POST['startNode40'];
+    $startNode41 = $_POST['startNode41'];
+    $startNode42 = $_POST['startNode42'];
+    $startNode43 = $_POST['startNode43'];
+    $startNode44 = $_POST['startNode44'];
+    $startNode45 = $_POST['startNode45'];
+    $startNode46 = $_POST['startNode46'];
+    $startNode47 = $_POST['startNode47'];
+    $startNode48 = $_POST['startNode48'];
     
     // Memeriksa ketersambungan antara simpul-simpul awal yang dipilih
     $isConnected1 = $graph->isConnected($startNode1, $startNode2);
