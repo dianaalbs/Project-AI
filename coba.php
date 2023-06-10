76f333a1aa9868049da2b74a4ef99d6bd15bb200
asyahra19 <asyahrara02@student.uns.ac.id>
2023-06-10 18:50:41 +0700

    edit coba

--- coba.php
+++ coba.php
@@ -10,58 +10,84 @@
         'Matkul A' => array(),
         'Matkul B' => array('Matkul A'),
         'Matkul C' => array('Matkul A'),
-        'Matkul D' => array('Matkul B', 'Matkul C')
+        'Matkul D' => array('Matkul B', 'Matkul C'),
+        'Matkul E' => array('Matkul D')
     );
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $selectedMatkuls = $_POST['matkuls'];
 
-        // Fungsi untuk mengecek prasyarat
-        function cekPrasyarat($matkul, $prasyarat, $selectedMatkuls) {
-            if (empty($prasyarat[$matkul])) {
-                return true; // Tidak memiliki prasyarat
-            } else {
+        if (count($selectedMatkuls) == 3) {
+            // Fungsi DFS untuk mengecek ketersambungan
+            function dfs($matkul, $prasyarat, &$visited) {
+                $visited[$matkul] = true;
+
                 foreach ($prasyarat[$matkul] as $p) {
-                    if (!in_array($p, $selectedMatkuls)) {
-                        return false; // Salah satu prasyarat belum dipilih
+                    if (!isset($visited[$p])) {
+                        dfs($p, $prasyarat, $visited);
                     }
                 }
-                return true; // Semua prasyarat terpenuhi
             }
-        }
 
-        // Menyimpan hasil pengecekan prasyarat untuk setiap mata kuliah
-        $hasilCekPrasyarat = array();
+            // Menyimpan hasil pengecekan prasyarat
+            $hasilCekPrasyarat = array();
 
-        // Cek prasyarat untuk setiap mata kuliah yang dipilih
-        foreach ($selectedMatkuls as $matkul) {
-            $hasilCekPrasyarat[$matkul] = cekPrasyarat($matkul, $prasyarat, $selectedMatkuls);
-        }
+            // Cek prasyarat untuk setiap mata kuliah yang dipilih
+            foreach ($selectedMatkuls as $matkul) {
+                $visited = array();
 
-        // Menampilkan hasil pengecekan prasyarat
-        echo "<h2>Hasil Pengecekan Prasyarat</h2>";
-        echo "<ul>";
-        foreach ($hasilCekPrasyarat as $matkul => $hasil) {
-            if ($hasil) {
-                echo "<li>Anda dapat mengambil mata kuliah $matkul.</li>";
-            } else {
-                echo "<li>Anda tidak dapat mengambil mata kuliah $matkul karena belum memenuhi prasyarat.</li>";
+                dfs($matkul, $prasyarat, $visited);
+
+                $hasilCekPrasyarat[$matkul] = count($visited) === count(array_keys($prasyarat));
             }
+
+            // Menampilkan hasil pengecekan prasyarat
+            echo "<h2>Hasil Pengecekan Prasyarat</h2>";
+            echo "<ul>";
+            foreach ($hasilCekPrasyarat as $matkul => $hasil) {
+                if ($hasil) {
+                    echo "<li>Anda tidak dapat mengambil mata kuliah $matkul karena belum memenuhi prasyarat.</li>";
+                } else {
+                    echo "<li>Anda dapat mengambil mata kuliah $matkul.</li>";
+                }
+            }
+            echo "</ul>";
+        } else {
+            echo "<h2>Pilih 3 mata kuliah.</h2>";
         }
-        echo "</ul>";
     }
     ?>
 
     <h2>Pilihan Mata Kuliah</h2>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <label for="matkuls">Pilih Mata Kuliah:</label><br>
-        <?php
-        $matakuliah = array('Matkul A', 'Matkul B', 'Matkul C', 'Matkul D');
-        foreach ($matakuliah as $matkul) {
-            echo '<input type="checkbox" name="matkuls[]" value="' . $matkul . '"> ' . $matkul . '<br>';
-        }
-        ?>
-        <br>
+        <label for="matkul1">Mata Kuliah 1:</label>
+        <select name="matkuls[]" id="matkul1">
+            <?php
+            $matakuliah = array('Matkul A', 'Matkul B', 'Matkul C', 'Matkul D', 'Matkul E');
+            foreach ($matakuliah as $matkul) {
+                echo '<option value="' . $matkul . '">' . $matkul . '</option>';
+            }
+            ?>
+        </select>
+        <br><br>
+        <label for="matkul2">Mata Kuliah 2:</label>
+        <select name="matkuls[]" id="matkul2">
+            <?php
+            foreach ($matakuliah as $matkul) {
+                echo '<option value="' . $matkul . '">' . $matkul . '</option>';
+            }
+            ?>
+        </select>
+        <br><br>
+        <label for="matkul3">Mata Kuliah 3:</label>
+        <select name="matkuls[]" id="matkul3">
+            <?php
+            foreach ($matakuliah as $matkul) {
+                echo '<option value="' . $matkul . '">' . $matkul . '</option>';
+            }
+            ?>
+        </select>
+        <br><br>
         <input type="submit" value="Cek Prasyarat">
     </form>
 </body>
